fca61947bb7f44e497a622808b993efcff9916f1
Keaton J. Burns <keaton.burns@gmail.com>
2020-10-24 18:28:00 -0700

    Add spectra plots

--- eady_dns/plot_spectra.py
+++ eady_dns/plot_spectra.py
@@ -2,10 +2,10 @@
 Plot planes from joint analysis files.
 
 Usage:
-    plot_slices.py <files>... [--output=<dir>]
+    plot_spectra.py <files>... [--output=<dir>]
 
 Options:
-    --output=<dir>  Output directory [default: ./frames]
+    --output=<dir>  Output directory [default: ./spectra]
 
 """
 
@@ -14,60 +14,60 @@ import numpy as np
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
+from mpi4py import MPI
 plt.ioff()
-from dedalus.extras import plot_tools
 
+import dedalus.public as de
+from dedalus.extras import plot_tools
+from parameters import *
+
+# Domain and scratch field
+x_basis = de.Fourier('x', Nx, interval=(0, L), dealias=3/2)
+y_basis = de.Fourier('y', Ny, interval=(0, L), dealias=3/2)
+z_basis = de.Chebyshev('z', Nz, interval=(0, H), dealias=3/2)
+domain = de.Domain([x_basis, y_basis, z_basis], grid_dtype=np.float64, comm=MPI.COMM_SELF)
+field = domain.new_field()
+
+def integrated_spectra(field, axis):
+    """Integrate power over specified axis."""
+    power = np.abs(field['c'])**2
+    return np.sum(power, axis=axis)
 
 def main(filename, start, count, output):
-    for loc, name in (('x=0', 'slice_x_0'),
-                      ('y=0', 'slice_y_0'),
-                      ("z='left'", 'slice_z_left'),
-                      ("z='center'", 'slice_z_center'),
-                      ("z='right'", 'slice_z_right')):
-        plot_slice(filename, start, count, output, loc, name)
+    plot_spectra(filename, start, count, output)
 
-def plot_slice(filename, start, count, output, loc, name):
+def plot_spectra(filename, start, count, output):
     """Save plot of specified tasks for given range of analysis writes."""
     # Plot settings
-    fields = ['b', 'u', 'v', 'w', 'ωz']
-    scale = 2.5
-    dpi = 100
+    tasks = ['b', 'u', 'v', 'w']
     title_func = lambda sim_time: 't = {:.3f}'.format(sim_time)
-    savename_func = lambda write: '{}_{:06}.png'.format(name, write)
+    savename_func = lambda write: 'spec_{:06}.png'.format(write)
     # Layout
-    if loc[0] == 'x':
-        nrows, ncols = 5, 1
-        image = plot_tools.Box(6, 1)
-        image_axes = (2, 3)
-        data_slices = (0, slice(None), slice(None))
-    elif loc[0] == 'y':
-        nrows, ncols = 5, 1
-        image = plot_tools.Box(6, 1)
-        image_axes = (1, 3)
-        data_slices = (slice(None), 0, slice(None))
-    elif loc[0] == 'z':
-        nrows, ncols = 2, 3
-        image = plot_tools.Box(2, 2)
-        image_axes = (1, 2)
-        data_slices = (slice(None), slice(None), 0)
-
+    nrows = 3
+    ncols = len(tasks)
+    image = plot_tools.Box(4, 4)
     pad = plot_tools.Frame(0.2, 0.2, 0.1, 0.1)
     margin = plot_tools.Frame(0.3, 0.2, 0.1, 0.1)
-
+    scale = 2
+    dpi = 100
     # Create multifigure
     mfig = plot_tools.MultiFigure(nrows, ncols, image, pad, margin, scale)
     fig = mfig.figure
     # Plot writes
     with h5py.File(filename, mode='r') as file:
         for index in range(start, start+count):
-            for n, field in enumerate(fields):
-                # Build subfigure axes
-                i, j = divmod(n, ncols)
-                axes = mfig.add_axes(i, j, [0, 0, 1, 1])
-                # Call plotting helper (dset axes: [t, x, y, z])
-                task = f"interp({field}, {loc})"
+            for j, task in enumerate(tasks):
                 dset = file['tasks'][task]
-                plot_tools.plot_bot(dset, image_axes, (index,)+data_slices, axes=axes, title=task, even_scale=True)
+                # Load field in grid layout
+                field.set_scales(1)
+                field['g'] = dset[index]
+                for i in range(3):
+                    data = np.log10(integrated_spectra(field, axis=i).T)
+                    image_axes = [0, 1, 2]
+                    image_axes.pop(i)
+                    # Build subfigure axes
+                    axes = mfig.add_axes(i, j, [0, 0, 1, 1])
+                    plot_tools.plot_bot(field, image_axes, data=data, axes=axes, title=task, even_scale=False,cmap='viridis')
             # Add time title
             title = title_func(file['scales/sim_time'][index])
             title_height = 1 - 0.5 * mfig.margin.top / mfig.fig.y
